2917c44f15da440550b07dccb8891d89caf9a68f
Chi Cuong Le <lecuongintum@gmail.com>
2021-12-20 23:55:43 +0000

    playing with array, slice

--- arrays/sum.go
+++ arrays/sum.go
@@ -7,3 +7,13 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(input ...[]int) []int {
+	//numberOfInput := len(input)
+	var result []int
+	//:= make([]int, numberOfInput)
+	for _, numbers := range input {
+		result = append(result, Sum(numbers))
+	}
+	return result
+}
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,6 +1,10 @@
 package arrays
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of 5 numbers", func(t *testing.T) {
@@ -21,4 +25,9 @@ func TestSum(t *testing.T) {
 		}
 	})
 
+}
+
+func TestSumAll(t *testing.T) {
+	assert.Equal(t, []int{3, 9}, SumAll([]int{1, 2}, []int{0, 9}), "summarize each array and return 2 sum values")
+	assert.Equal(t, []int{3, 9, 5}, SumAll([]int{1, 2}, []int{0, 9}, []int{1, 2, 2}), "number of parameters are variable")
 }
