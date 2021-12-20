092a00c7f8231f6a0277d9d22119b0784b952ac2
Chi Cuong Le <lecuongintum@gmail.com>
2021-12-20 22:58:58 +0000

    closure environments

--- functions/closure_test.go
+++ functions/closure_test.go
@@ -19,7 +19,9 @@ func TestEvenGenerator(t *testing.T) {
 	t.Run("function returned by makeEvenGenerator is combined with variable init. the init varialbe is persisted between calls", func(t *testing.T) {
 		nextEven := makeEvenGenerator()
 		assert.Equal(t, uint(0), nextEven())
-		assert.Equal(t, uint(2), nextEven())
+		assert.Equal(t, uint(2), nextEven(), "nextEven is a closure with a specific environment that is retain, persist")
 		assert.Equal(t, uint(4), nextEven())
+		newNextEven := makeEvenGenerator()
+		assert.Equal(t, uint(0), newNextEven(), " newNextEven is completely new initialized therefore init value is reset to 0")
 	})
 }
