bae74a4abf0993a834062e8d196cc0aa5291a3fb
Chi Cuong Le <lecuongintum@gmail.com>
2021-12-21 00:29:09 +0000

    building an abstract shape struct/object with functions and two sub struct of shape

--- datastructure/struct_test.go
+++ datastructure/struct_test.go
@@ -1,14 +1,44 @@
 package datastructure
 
 import (
+	"math"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+type shape interface {
+	Area() float64
+	Perimeter() float64
+}
+
+func getArea(s shape) float64 {
+	return s.Area()
+}
+
 type Rectangle struct {
 	heigth float64
 	width  float64
+}
+
+func (r *Rectangle) Area() float64 {
+	return r.heigth * r.width
+}
+
+func (r *Rectangle) Perimeter() float64 {
+	return 2 * (r.heigth + r.width)
+}
+
+type Circle struct {
+	radius float64
+}
+
+func (c *Circle) Area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func (c *Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.radius
 }
 
 func TestPerimeter(t *testing.T) {
@@ -21,11 +51,13 @@ func TestPerimeter(t *testing.T) {
 		assert.Equal(t, 72.0, rectangle.Area())
 	})
 }
-
-func (r *Rectangle) Area() float64 {
-	return r.heigth * r.width
-}
-
-func (r *Rectangle) Perimeter() float64 {
-	return 2 * (r.heigth + r.width)
+func TestShape(t *testing.T) {
+	t.Run("using for rectangle", func(t *testing.T) {
+		rectangle := &Rectangle{10.0, 10.0}
+		assert.Equal(t, 100.0, getArea(rectangle))
+	})
+	t.Run("using for a circle", func(t *testing.T) {
+		rectangle := &Circle{10.0}
+		assert.InEpsilon(t, 314.15, getArea(rectangle), float64(0.001))
+	})
 }
